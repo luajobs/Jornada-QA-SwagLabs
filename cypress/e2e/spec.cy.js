61492f3c780c182823e06a87302485e4088a4f3c
luajobs <luanaaraujobarros8@gmail.com>
2025-04-03 19:45:21 -0300

    It
    Login válido

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Login Válido', () => {
     cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('.product_label').contains("Products")
   })
 })
