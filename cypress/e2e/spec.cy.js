34c83f29658b26c7436a80490d096a68b043b0a7
luajobs <luanaaraujobarros8@gmail.com>
2025-04-03 20:38:47 -0300

    test
    Rodando os dois testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,12 +7,12 @@ describe('template spec', () => {
     cy.get('.product_label').contains("Products")
   })
 
-  it.only('Login inválido', () => {
+  it('Login inválido', () => {
     cy.visit('https://www.saucedemo.com/v1/')
     cy.get('[data-test="username"]').type('moon_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('#login-button').click()
-    
+    cy.get('[data-test="error"]').contains("Epic sadface: Username and password do not match any user in this service")
 
   })
 })
